f0b91baebde73290d85f51a760e2e05bae5fe073
Ricky Yuan <rickyyuan07@gmail.com>
2025-12-11 02:49:42 +0000

    upload some stuff

--- examples/wanvideo/model_inference/Wan2.1-VACE-1.3B.py
+++ examples/wanvideo/model_inference/Wan2.1-VACE-1.3B.py
@@ -15,8 +15,6 @@ pipe = WanVideoPipeline.from_pretrained(
     ],
 )
 
-pipe.enable_vram_management()
-
 # dataset_snapshot_download(
 #     dataset_id="DiffSynth-Studio/examples_in_diffsynth",
 #     local_dir="./",
@@ -53,12 +51,24 @@ pipe.enable_vram_management()
 # save_video(video, "video3.mp4", fps=15, quality=5)
 
 # gray video -> Video (inpainting)
-control_video = VideoData("data/sample/preprocess/vace_video.mp4", height=480, width=832)
-mask_video = VideoData("data/sample/preprocess/vace_video_mask.mp4", height=480, width=832)
+pipe.load_lora(pipe.vace, "models/train/Wan2.1-VACE-1.3B_lora/epoch-3.safetensors", alpha=1)
+pipe.enable_vram_management()
+
+control_video = VideoData("data/example3/vid1_vace_video.mp4", height=480, width=832)
+mask_video = VideoData("data/example3/vid1_vace_video_mask.mp4", height=480, width=832)
+# control_video = VideoData("data/example_video_dataset/masked_output_gray.mp4", height=480, width=832)
+# mask_video = VideoData("data/example_video_dataset/binary_mask_output.mp4", height=480, width=832)
 video = pipe(
-    prompt="a photorealistic, cinematic, high-fashion commercial, 360° video of a toy rubber duck as the main product and other toys around it",
+    prompt="""A full-sized fashion doll wearing a blue hoodie with a [V] white circular geometric logo centered on the chest,
+            is standing upright in the middle of a brightly lit toyhouse play area. The background features colorful foam play 
+            mats on the floor, miniature furniture, a slide, and various toys and pastel-colored storage shelves. The doll is 
+            placed in the center foreground, facing the camera, with the camera moving softly in a front-facing arc around the 
+            doll to keep the logo visible.""",
+    negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
+    # prompt="a photorealistic, cinematic, high-fashion commercial, a toy rubber duck floating on the swimming pool with the baby.",
     vace_video=control_video,
     vace_video_mask=mask_video,
+    # vace_reference_image=Image.open("data/example3/ref_image.jpg").resize((832, 480)),
     seed=1, tiled=True
 )
-save_video(video, "data/sample/video_output.mp4", fps=24, quality=9)
+save_video(video, "data/example3/video_output1.mp4", fps=24, quality=9)
--- examples/wanvideo/model_training/validate_lora/Wan2.1-VACE-1.3B.py
+++ examples/wanvideo/model_training/validate_lora/Wan2.1-VACE-1.3B.py
@@ -6,13 +6,14 @@ from diffsynth import save_video, VideoData
 from diffsynth.pipelines.wan_video_new import WanVideoPipeline, ModelConfig
 from tqdm import tqdm
 
+seed = 42
 np.random.seed(42)
-output_dir = "data/example3/outputs/5epochs_32_rank"
-input_dir = "data/example3/preprocess_target/"
-df = pd.read_csv("data/example3/preprocess_target/metadata_target.csv")
+output_dir = "data/target1/outputs/baseline/"
+input_dir = "data/target1/"
+df = pd.read_csv("data/target1/metadata.csv")
 os.makedirs(output_dir, exist_ok=True)
 num_inference = 1
-lora_path = "models/train/Wan2.1-VACE-1.3B_lora/example3/5epochs/epoch-4.safetensors"
+lora_path = "models/train/Wan2.1-VACE-1.3B_lora/example1/ohwx/epoch-4.safetensors"
 
 pipe = WanVideoPipeline.from_pretrained(
     torch_dtype=torch.bfloat16,
@@ -27,22 +28,25 @@ pipe = WanVideoPipeline.from_pretrained(
 pipe.load_lora(pipe.vace, lora_path, alpha=1)
 pipe.enable_vram_management()
 
-print(f'starting inference')
+print(f'starting inference for {len(df)} videos...')
 for index, row in tqdm(df.iterrows()):
     print(f'inferrring {row["video"]}...')
     control_video_path = os.path.join(input_dir, row["vace_video"])
     control_video = VideoData(control_video_path, height=480, width=832)
     mask_video_path = os.path.join(input_dir, row["vace_video_mask"])
     mask_video = VideoData(mask_video_path, height=480, width=832)
+    video_path = os.path.join(input_dir, row["video"])
+    reference_image = VideoData(video_path, height=480, width=832)[0]
     prompt = row["prompt"]
 
     for i in range(num_inference):
-        seed = np.random.randint(1, 10000001)
+        # seed = np.random.randint(1, 10000001)
         video = pipe(
             prompt=prompt,
             negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
             vace_video=control_video,
             vace_video_mask=mask_video,
+            vace_reference_image=reference_image,
             seed=seed, tiled=True
         )
         save_video(video, f"{output_dir}/{row['video'].split('.')[0]}_seed_{seed}.mp4", fps=24, quality=9)
